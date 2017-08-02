479dc4236eb4c0d7838ce55c450d3e0cbb517e71
Leo Cookson <10001481@BCS.NET.NZ>
2017-08-03 10:25:12 +1200

    First

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+			
+         Console.WriteLine("Please enter a number");
+         var Input = Console.ReadLine();
+
+         var number = 0;
+         var isnum = int.TryParse(Input, out number);
+
+         if(isnum){
+            int count = 12;
+            for (int i = 0; i <= count; i++)
+            Console.WriteLine(Input + "X" +i);
+            var Total = 78;
+          Console.WriteLine(Input+ Total);
+
+          
+           
+         }
+         else{
+             Console.WriteLine("Please enter a number ");
+         }
+
+
+
+
+
+
+
+
+
+			Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+    }
+}
